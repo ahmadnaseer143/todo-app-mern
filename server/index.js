507825cf96f4f802fd117d74cb6c663d4fe62fdb
ahmad.naseer143 <anaseer942@gmail.com>
2023-03-06 14:09:02 +0500

    backend complete

--- server/index.js
+++ server/index.js
@@ -1,14 +1,30 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+
+// Connect to the database
+mongoose
+  .connect(process.env.DB, { useNewUrlParser: true })
+  .then(() => console.log(`Database connected successfully`))
+  .catch((err) => console.log(err));
+// Since mongoose's Promise is deprecated, we override it with Node's Promise
+mongoose.Promise = global.Promise;
+
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+
+app.use(cors());
+app.use(bodyParser.json());
 
 // all routers
 const apiRouter = require("./routes/api");
@@ -20,7 +36,6 @@ app.use((err, req, res, next) => {
   console.log(err);
   next();
 });
-
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
